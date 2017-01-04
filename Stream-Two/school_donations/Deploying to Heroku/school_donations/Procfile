db7c1628fde8900f429457cab9cb325630114afc
Niel <nielmce@gmail.com>
2017-01-04 12:46:10 +0000

    Add stream2 project and heroku deployment seperately

---
+++ Stream-Two/school_donations/Deploying to Heroku/school_donations/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn school_donations_:app
---
+++ Stream-Two/school_donations/Final Project for Stream Two/school_donations/static/js/graph.js
@@ -0,0 +1,134 @@
+queue()
+    .defer(d3.json, "/donorsUS/projects")
+    .await(makeGraphs);
+
+function makeGraphs(error, projectsJson) {
+
+    //Clean projectsJson data
+    var donorsUSProjects = projectsJson;
+    var dateFormat = d3.time.format("%Y-%m-%d %H:%M:%S");
+    donorsUSProjects.forEach(function (d) {
+        d["date_posted"] = dateFormat.parse(d["date_posted"]);
+        d["date_posted"].setDate(1);
+        d["total_donations"] = +d["total_donations"];
+    });
+
+    //Create a Crossfilter instance
+    var ndx = crossfilter(donorsUSProjects);
+
+    //Define Dimensions
+    var dateDim = ndx.dimension(function (d) {
+        return d["date_posted"];
+    });
+    var resourceTypeDim = ndx.dimension(function (d) {
+        return d["resource_type"];
+    });
+    var povertyLevelDim = ndx.dimension(function (d) {
+        return d["poverty_level"];
+    });
+    var stateDim = ndx.dimension(function (d) {
+        return d["school_state"];
+    });
+    var totalDonationsDim = ndx.dimension(function (d) {
+        return d["total_donations"];
+    });
+
+    var fundingStatus = ndx.dimension(function (d) {
+        return d["funding_status"];
+    });
+
+
+    //Calculate metrics
+    var numProjectsByDate = dateDim.group();
+    var numProjectsByResourceType = resourceTypeDim.group();
+    var numProjectsByPovertyLevel = povertyLevelDim.group();
+    var numProjectsByFundingStatus = fundingStatus.group();
+    var totalDonationsByState = stateDim.group().reduceSum(function (d) {
+        return d["total_donations"];
+    });
+    var stateGroup = stateDim.group();
+
+
+    var all = ndx.groupAll();
+    var totalDonations = ndx.groupAll().reduceSum(function (d) {
+        return d["total_donations"];
+    });
+
+    var max_state = totalDonationsByState.top(1)[0].value;
+
+    //Define values (to be used in charts)
+    var minDate = dateDim.bottom(1)[0]["date_posted"];
+    var maxDate = dateDim.top(1)[0]["date_posted"];
+
+    //Charts
+    var timeChart = dc.lineChart("#time-chart");
+    var resourceTypeChart = dc.rowChart("#resource-type-row-chart");
+    var povertyLevelChart = dc.rowChart("#poverty-level-row-chart");
+    var numberProjectsND = dc.numberDisplay("#number-projects-nd");
+    var totalDonationsND = dc.numberDisplay("#total-donations-nd");
+    var fundingStatusChart = dc.pieChart("#funding-chart");
+
+
+    selectField = dc.selectMenu('#menu-select')
+        .dimension(stateDim)
+        .group(stateGroup);
+
+    numberProjectsND
+        .formatNumber(d3.format("d"))
+        .valueAccessor(function (d) {
+            return d;
+        })
+        .group(all);
+
+    totalDonationsND
+        .formatNumber(d3.format("d"))
+        .valueAccessor(function (d) {
+            return d;
+        })
+        .group(totalDonations)
+        .formatNumber(d3.format(".3s"));
+
+    timeChart
+        .ordinalColors(["#C96A23"])
+        .width(1200)
+        .height(300)
+        .margins({top: 30, right: 50, bottom: 30, left: 50})
+        .dimension(dateDim)
+        .group(numProjectsByDate)
+        .renderArea(true)
+        .transitionDuration(500)
+        .x(d3.time.scale().domain([minDate, maxDate]))
+        .elasticY(true)
+        .xAxisLabel("Year")
+        .yAxis().ticks(6);
+
+
+    resourceTypeChart
+        .ordinalColors(["#79CED7", "#66AFB2", "#C96A23", "#D3D1C5", "#F5821F"])
+        .width(300)
+        .height(250)
+        .dimension(resourceTypeDim)
+        .group(numProjectsByResourceType)
+        .xAxis().ticks(4);
+
+    povertyLevelChart
+        .ordinalColors(["#79CED7", "#66AFB2", "#C96A23", "#D3D1C5", "#F5821F"])
+        .width(300)
+        .height(250)
+        .dimension(povertyLevelDim)
+        .group(numProjectsByPovertyLevel)
+        .xAxis().ticks(4);
+
+    fundingStatusChart
+        .ordinalColors(["#79CED7", "#66AFB2", "#C96A23", "#D3D1C5", "#F5821F"])
+        .height(220)
+        .radius(90)
+        .innerRadius(40)
+        .transitionDuration(1500)
+        .dimension(fundingStatus)
+        .group(numProjectsByFundingStatus);
+
+
+    dc.renderAll();
+
+}
