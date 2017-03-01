23ab6d48ffa51e900c0e7dace9e82229b377f149
Yoni Lavi <yoni@codeinstitute.net>
2017-03-01 19:12:21 +0000

    Bring back the lower limit

--- Stream-Two/school_donations/Deploying_to_Heroku/school_donations/school_donations_.py
+++ Stream-Two/school_donations/Deploying_to_Heroku/school_donations/school_donations_.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
-DBS_NAME = os.getenv('MONGO_DB_NAME','donorsUSA')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'donorsUSA')
 COLLECTION_NAME = 'projects'
 
 @app.route("/")
@@ -36,11 +36,10 @@ def donor_projects():
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
-        # and limit the the results to 55000
-        projects = collection.find(projection=FIELDS, limit=55000)
+        # and limit the the results to a lower limit of 20000
+        projects = collection.find(projection=FIELDS, limit=20000)
         # Convert projects to a list in a JSON object and return the JSON data
         return json.dumps(list(projects))
-
 
 
 if __name__ == "__main__":
