afc9b109412f7a88829f0019b70188bee3cefab6
Yoni Lavi <yoni@codeinstitute.net>
2017-03-01 18:44:23 +0000

    Refactor the code to be nicer and add comments

--- Stream-Two/school_donations/Final_Project_for_Stream_Two/school_donations/school_donations_.py
+++ Stream-Two/school_donations/Final_Project_for_Stream_Two/school_donations/school_donations_.py
@@ -9,24 +9,40 @@ MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 DBS_NAME = 'donorsUSA'
 COLLECTION_NAME = 'projects'
-FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
-          'date_posted': True, 'total_donations': True, '_id': False}
 
 
 @app.route("/")
 def index():
+    """
+    A Flask view to serve the main dashboard page.
+    """
     return render_template("index.html")
 
 
 @app.route("/donorsUS/projects")
 def donor_projects():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS, limit=55000)
-    json_projects = list(projects)
-    json_projects = json.dumps(json_projects)
-    connection.close()
-    return json_projects
+    """
+    A Flask view to serve the project data from
+    MongoDB in JSON format.
+    """
+
+    # A constant that defines the record fields that we wish to retrieve.
+    FIELDS = {
+        '_id': False, 'funding_status': True, 'school_state': True,
+        'resource_type': True, 'poverty_level': True,
+        'date_posted': True, 'total_donations': True
+    }
+
+    # Open a connection to MongoDB using a with statement such that the
+    # connection will be closed as soon as we exit the with statement
+    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+        # Define which collection we wish to access
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        # Retrieve a result set only with the fields defined in FIELDS
+        # and limit the the results to 55000
+        projects = collection.find(projection=FIELDS, limit=55000)
+        # Convert projects to a list in a JSON object and return the JSON data
+        return json.dumps(list(projects))
 
 
 if __name__ == "__main__":
