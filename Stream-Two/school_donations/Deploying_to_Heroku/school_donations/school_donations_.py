658268c65c3f7d270ec034150d9c0b8ad0b2a36c
Yoni Lavi <yoni@codeinstitute.net>
2017-03-01 19:08:26 +0000

    Change the formatting and add a localhost default
    Change the formatting to fit the new one in the previous example, as well as add a default to MONGO_URI to work on localhost

--- Stream-Two/school_donations/Deploying_to_Heroku/school_donations/school_donations_.py
+++ Stream-Two/school_donations/Deploying_to_Heroku/school_donations/school_donations_.py
@@ -6,14 +6,9 @@ import os
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
 DBS_NAME = os.getenv('MONGO_DB_NAME','donorsUSA')
-MONGO_URI = os.getenv('MONGODB_URI')
 COLLECTION_NAME = 'projects'
-FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
-          'date_posted': True, 'total_donations': True, '_id': False}
-
 
 @app.route("/")
 def index():
@@ -22,14 +17,30 @@ def index():
 
 @app.route("/donorsUS/projects")
 def donor_projects():
-    connection = MongoClient(MONGO_URI)
-    # This connection is required when hosted using a remote mongo db.
-    collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS, limit=55000)
-    json_projects = list(projects)
-    json_projects = json.dumps(json_projects)
-    connection.close()
-    return json_projects
+    """
+    A Flask view to serve the project data from
+    MongoDB in JSON format.
+    """
+
+    # A constant that defines the record fields that we wish to retrieve.
+    FIELDS = {
+        '_id': False, 'funding_status': True, 'school_state': True,
+        'resource_type': True, 'poverty_level': True,
+        'date_posted': True, 'total_donations': True
+    }
+
+    # Open a connection to MongoDB using a with statement such that the
+    # connection will be closed as soon as we exit the with statement
+    # The MONGO_URI connection is required when hosted using a remote mongo db.
+    with MongoClient(MONGO_URI) as conn:
+        # Define which collection we wish to access
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        # Retrieve a result set only with the fields defined in FIELDS
+        # and limit the the results to 55000
+        projects = collection.find(projection=FIELDS, limit=55000)
+        # Convert projects to a list in a JSON object and return the JSON data
+        return json.dumps(list(projects))
+
 
 
 if __name__ == "__main__":
