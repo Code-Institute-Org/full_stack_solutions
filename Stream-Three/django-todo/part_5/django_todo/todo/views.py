dbc2929776994b08a45bf15460b44ad840a0eb7e
Yoni Lavi <yoni@codeinstitute.net>
2017-09-20 03:05:00 +0100

    fixed typo in permission_classes

--- Stream-Three/django-todo/part_5/django_todo/todo/views.py
+++ Stream-Three/django-todo/part_5/django_todo/todo/views.py
@@ -15,7 +15,7 @@ class TodoView(APIView):
     `todo` items
     """
 
-    permissions_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request, pk=None):
         """
